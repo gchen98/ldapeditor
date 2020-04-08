8da1ba147bc9a395060d4ce59b8f03180b6a8f9c
Gary Chen <gchen98@gmail.com>
2020-04-08 09:13:22 -0700

    Added Elise and Julian

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,7 @@
-cp target/ldapeditor.war /opt/java/apache-tomcat-7.0.53/webapps/
+#!/bin/bash
+
+sudo systemctl stop tomcat8
+TOMCAT_HOME=/var/lib/tomcat8
+sudo -u tomcat8 rm -fr ${TOMCAT_HOME}/webapps/ldapeditor
+sudo -u tomcat8 cp target/ldapeditor.war ${TOMCAT_HOME}/webapps/
+sudo systemctl start tomcat8
